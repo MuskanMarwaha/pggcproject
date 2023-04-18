679fdf64da7c17a3720cc67c5f0133d0d6cf9c6d
MuskanMarwaha <131140425+MuskanMarwaha@users.noreply.github.com>
2023-04-18 23:24:49 +0530

    Update signin.php

--- signin.php
+++ signin.php
@@ -2,11 +2,11 @@
 <?php include "phpvalidation.php" ?>
 
 <!DOCTYPE html>
-<!-- Created By CodingLab - www.codinglabweb.com -->
+
 <html lang="en" dir="ltr">
   <head>
     <meta charset="UTF-8">
-    <!---<title> Responsive Registration Form | CodingLab </title>--->
+   
     <link rel="stylesheet" href="s.css">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
      <style>.error{color: red;}</style>
